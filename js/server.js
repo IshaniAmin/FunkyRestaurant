71309a3231b6ad2a6361b5e3c4d00a978c9fd9ff
rob bee <rbenny@earthlink.net>
2017-02-11 13:23:09 -0500

    update 2

--- js/server.js
+++ js/server.js
@@ -1,1 +1,73 @@
-server.js
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Star Wars Characters (DATA)
+// =============================================================
+var reservation = [{
+ 
+ 
+}];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+// app.get("/api/:characters?", function(req, res) {
+//   var chosen = req.params.characters;
+
+//   if (chosen) {
+//     console.log(chosen);
+
+//     for (var i = 0; i < characters.length; i++) {
+//       if (chosen === characters[i].routeName) {
+//         res.json(characters[i]);
+//         return;
+//       }
+//     }
+
+//     res.json(false);
+//   }
+//   else {
+//     res.json(characters);
+//   }
+// });
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newreserve = req.body;
+
+  console.log(newreserve);
+
+  reservation.push(newreserve);
+
+  res.json(reservation);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
